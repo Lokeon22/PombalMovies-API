ad40909d2644af809926f4d3fb0575835dd2b670
Gabriel Felipe <gabrielfelipe941@yahoo.com.br>
2022-09-08 18:52:11 -0300

    tagMovie bug resolved

--- src/controllers/MovieNotesController.js
+++ src/controllers/MovieNotesController.js
@@ -12,11 +12,16 @@ class MovieNotesController {
       user_id,
     });
 
-    await knex("movieTags").insert({
-      name,
-      user_id,
-      note_id: movienotes_id,
+    // Map criado para renderizar varias tags como uma array [ ] name é a tag
+    const tagsInsert = name.map((name) => {
+      return {
+        note_id: movienotes_id,
+        user_id,
+        name,
+      };
     });
+
+    await knex("movieTags").insert(tagsInsert);
 
     res.json({ status: "Nota criada" });
   }
@@ -34,20 +39,34 @@ class MovieNotesController {
   }
 
   async index(req, res) {
-    const { title } = req.query;
+    const { title, name } = req.query;
     const user_id = req.user.id;
 
-    const index = await knex("movieNotes")
-      .where({ user_id })
-      .whereLike("title", `%${title}%`)
-      .orderBy("title");
+    let notes;
+
+    if (name) {
+      const filterNames = name.split(",").map((tag) => tag.trim());
+
+      notes = await knex("movieTags")
+        .select(["movieNotes.id", "movieNotes.title", "movieNotes.user_id"])
+        .where("movieNotes.user_id", user_id)
+        .whereLike("movieNotes.title", `%${title}%`)
+        .whereIn("name", filterNames)
+        .innerJoin("movieNotes", "movieNotes.id", "movieTags.note_id")
+        .orderBy("notes.title");
+    } else {
+      notes = await knex("movieNotes")
+        .where({ user_id })
+        .whereLike("title", `%${title}%`)
+        .orderBy("title");
+    }
     //Abaixo está sendo feito o filtro para retornar a tag relacionada ao titulo
     const movieTags = await knex("movieTags").where({ user_id });
-    const movieNotesTags = index.map((note) => {
+    const movieNotesTags = notes.map((note) => {
       const noteTags = movieTags.filter((tag) => tag.note_id === note.id);
 
       return {
-        ...index,
+        ...note,
         tags: noteTags,
       };
     });
